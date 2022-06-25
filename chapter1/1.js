e79f860e50b5df8315c06173d15e8c94926268f8
parsee1053 <s.naiki.parsee1053@gmail.com>
2022-06-25 12:47:00 +0900

    演習問題1-1

---
+++ chapter1/1.js
@@ -0,0 +1,13 @@
+console.log(NaN + 0); // NaN
+console.log(Infinity + 0); // Infinity
+console.log(false + 0); // 0
+console.log(true + 0); // 1
+console.log(null + 0); // 0
+console.log(undefined + 0); // NaN
+
+console.log('' + NaN); // 'NaN'(文字列)
+console.log('' + Infinity); // 'Infinity'(文字列)
+console.log('' + false); // 'false'(文字列)
+console.log('' + true); // 'true'(文字列)
+console.log('' + null); // 'null'(文字列)
+console.log('' + undefined); // 'undefined'(文字列)
